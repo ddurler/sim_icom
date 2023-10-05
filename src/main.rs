34761c0425e6a64436fa0a993fa93af1e9bf5c8b
DDurler <denis.durler@gmail.com>
2023-10-05 20:20:17 +0200

    Add tokio

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,52 @@
 use std::sync::{Arc, Mutex};
-use std::thread;
-use std::time::Duration;
 
 mod database;
-use database::Database;
+use database::{Database, IdTag};
 
-fn main() {
+async fn my_test_process(thread_db: Arc<Mutex<Database>>, option_id_tag: Option<IdTag>) {
+    let id_user;
+    {
+        // Verrouiller la database partagée pour accéder à sa valeur
+        let mut db = thread_db.lock().unwrap();
+
+        // Obtient un id_user pour les opérations
+        id_user = db.get_id_user();
+    }
+
+    // Laisse la main...
+    tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+
+    {
+        // Verrouiller la database partagée pour accéder à sa valeur
+        let mut db = thread_db.lock().unwrap();
+
+        if let Some(id_tag) = option_id_tag {
+            // Modifier une valeur (méthode via l'id_tag)
+            let value = db.get_u8_from_id_tag(id_user, id_tag);
+            db.set_u8_to_word_address(id_user, 0, value + 20);
+        } else {
+            // Modifier une valeur (méthode via l'adresse)
+            let value = db.get_u8_from_word_address(id_user, 0);
+            db.set_u8_to_word_address(id_user, 0, value + 10);
+        }
+    }
+
+    // Laisse la main...
+    tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+
+    {
+        // Verrouiller la database partagée pour accéder à sa valeur
+        let mut db = thread_db.lock().unwrap();
+
+        // Voir s'il y a un notification d'un autre utilisateur
+        if let Some(tag) = db.get_change(id_user, false, false) {
+            println!("User #{id_user} notifié du changement de {tag}");
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
     // Initialisation de la database
     // let db = Database::from_file("./datas/database_test.csv");
     let mut db: Database = Database::from_file("./datas/database.csv");
@@ -29,78 +70,12 @@ fn main() {
     let thread2_data = Arc::clone(&shared_db);
 
     // Créer les threads
-    let thread1 = thread::spawn(move || {
-        let id_user;
-        {
-            // Verrouiller la database partagée pour accéder à sa valeur
-            let mut db = thread1_data.lock().unwrap();
-
-            // Obtient un id_user pour les opérations
-            id_user = db.get_id_user();
-        }
-
-        std::thread::sleep(Duration::from_millis(100));
-
-        {
-            // Verrouiller la database partagée pour accéder à sa valeur
-            let mut db = thread1_data.lock().unwrap();
-
-            // Modifier une valeur (méthode via l'id_tag)
-            let value = db.get_u8_from_word_address(id_user, 0);
-            db.set_u8_to_word_address(id_user, 0, value + 10);
-        }
-
-        std::thread::sleep(Duration::from_millis(100));
-
-        {
-            // Verrouiller la database partagée pour accéder à sa valeur
-            let mut db = thread1_data.lock().unwrap();
-
-            // Voir s'il y a un notification d'un autre utilisateur
-            if let Some(tag) = db.get_change(id_user, false, false) {
-                println!("User #{id_user} notifié du changement de {tag}");
-            }
-        }
-
-    });
-
-    let thread2 = thread::spawn(move || {
-        let id_user;
-        {
-            // Verrouiller la database partagée pour accéder à sa valeur
-            let mut db = thread2_data.lock().unwrap();
-
-            // Obtient un id_user pour les opérations
-            id_user = db.get_id_user();
-        }
-
-        std::thread::sleep(Duration::from_millis(100));
-
-        {
-            // Verrouiller la database partagée pour accéder à sa valeur
-            let mut db = thread2_data.lock().unwrap();
-
-            // Modifier une valeur (méthode via l'id_tag)
-            let value = db.get_u8_from_id_tag(id_user, id_tag);
-            db.set_u8_to_word_address(id_user, 0, value + 20);
-        }
-
-        std::thread::sleep(Duration::from_millis(100));
-
-        {
-            // Verrouiller la database partagée pour accéder à sa valeur
-            let mut db = thread2_data.lock().unwrap();
-
-            // Voir s'il y a un notification d'un autre utilisateur
-            if let Some(tag) = db.get_change(id_user, false, false) {
-                println!("User #{id_user} notifié du changement de {tag}");
-            }
-        }
-    });
+    let handle_1 = tokio::spawn(async move { my_test_process(thread1_data, None).await });
+    let handle_2 = tokio::spawn(async move { my_test_process(thread2_data, Some(id_tag)).await });
 
     // Attendre que les threads se terminent
-    thread1.join().unwrap();
-    thread2.join().unwrap();
+    handle_1.await.unwrap();
+    handle_2.await.unwrap();
 
     // Accéder à la valeur finale de la zone de données partagée
     let db = shared_db.lock().unwrap();
