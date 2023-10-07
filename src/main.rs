cb719bc5ac1e73611cd9f76a51a1441ca072d08b
DDurler <denis.durler@gmail.com>
2023-10-07 22:39:41 +0200

    feat: Add watcher

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,21 @@ use tokio::net::TcpListener;
 use tokio_modbus::server::tcp::{accept_tcp_connection, Server};
 
 mod database;
-use database::{Database, IdTag};
+use database::Database;
 
 mod server_modbus_tcp;
 use server_modbus_tcp::DatabaseService;
 
-async fn my_test_process(thread_db: Arc<Mutex<Database>>, option_id_tag: Option<IdTag>) {
+/// Routine d'un thread qui trace les modifications effectuées dans la [`Database`]
+/// En paramètre, le temps de cycle entre chaque trace (en millisecondes)
+/// Et un booléen pour indiquer si on trace également les modifications 'anonymes'
+async fn database_watcher(
+    thread_db: Arc<Mutex<Database>>,
+    cycle_in_msecs: u64,
+    include_anonymous_changes: bool,
+) {
+    println!("WATCHER: Starting...");
+
     let id_user;
     {
         // Verrouiller la database partagée pour accéder à sa valeur
@@ -20,38 +29,24 @@ async fn my_test_process(thread_db: Arc<Mutex<Database>>, option_id_tag: Option<
         id_user = db.get_id_user();
     }
 
-    // Laisse la main...
-    tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+    loop {
+        loop {
+            // Verrouiller la database partagée pour accéder à sa valeur
+            let mut db = thread_db.lock().unwrap();
 
-    {
-        // Verrouiller la database partagée pour accéder à sa valeur
-        let mut db = thread_db.lock().unwrap();
-
-        if let Some(id_tag) = option_id_tag {
-            // Modifier une valeur (méthode via l'id_tag)
-            let value = db.get_u8_from_id_tag(id_user, id_tag);
-            db.set_u8_to_word_address(id_user, 0, value + 20);
-        } else {
-            // Modifier une valeur (méthode via l'adresse)
-            let value = db.get_u8_from_word_address(id_user, 0);
-            db.set_u8_to_word_address(id_user, 0, value + 10);
+            // Voir s'il y a un notification d'un autre utilisateur
+            if let Some(tag) = db.get_change(id_user, false, include_anonymous_changes) {
+                println!(
+                    "WATCHER: {} = {}",
+                    tag,
+                    db.get_t_value_from_tag(id_user, &tag)
+                );
+            } else {
+                break;
+            }
         }
-    }
-
-    for _ in 0..300 {
-        /* Pendant 5 minutes... */
         // Laisse la main...
-        tokio::time::sleep(tokio::time::Duration::from_millis(1000)).await;
-
-        // Verrouiller la database partagée pour accéder à sa valeur
-        let mut db = thread_db.lock().unwrap();
-
-        // Voir s'il y a un notification d'un autre utilisateur
-        if let Some(tag) = db.get_change(id_user, false, false) {
-            println!("User #{id_user} notifié du changement de {tag}");
-        } else {
-            println!("Pas de changement...");
-        }
+        tokio::time::sleep(tokio::time::Duration::from_millis(cycle_in_msecs)).await;
     }
 }
 
@@ -60,27 +55,18 @@ async fn main() -> anyhow::Result<()> {
     // Initialisation de la database
     let mut db: Database = Database::from_file("./datas/database.csv");
 
-    // Obtient un id_user pour les opérations
+    // Extrait un id_user pour le serveur
     let id_user = db.get_id_user();
-
-    // Choisi l'id_tag d'un tag à une adresse MODBUS
-    let id_tag = db.get_tag_from_word_address(0x0).unwrap().id_tag;
-
-    println!(
-        "Valeur initiale : {}",
-        db.get_u8_from_word_address(id_user, 0)
-    );
 
     // Créer la database partagée mutable
     let shared_db = Arc::new(Mutex::new(db));
 
     // Cloner la référence à la database partagée pour chaque thread
-    let thread1_data = Arc::clone(&shared_db);
-    let thread2_data = Arc::clone(&shared_db);
+    let db_watcher = Arc::clone(&shared_db);
 
-    // Créer les threads
-    let handle_1 = tokio::spawn(async move { my_test_process(thread1_data, None).await });
-    let handle_2 = tokio::spawn(async move { my_test_process(thread2_data, Some(id_tag)).await });
+    // Créer un watcher
+    let handle_watcher =
+        tokio::spawn(async move { database_watcher(db_watcher, 1000, true).await });
 
     // Serveur MODBUS
     let socket_addr: SocketAddr = "127.0.0.1:502".parse().unwrap();
@@ -98,19 +84,9 @@ async fn main() -> anyhow::Result<()> {
         eprintln!("{err}");
     };
     server.serve(&on_connected, on_process_error).await?;
-    // Ok(())
 
     // Attendre que les threads se terminent
-    handle_1.await.unwrap();
-    handle_2.await.unwrap();
-
-    // Accéder à la valeur finale de la zone de données partagée
-    let db = shared_db.lock().unwrap();
-    // println!("db = {db}");
-    println!(
-        "Valeur finale : {}",
-        db.get_u8_from_word_address(id_user, 0)
-    );
+    handle_watcher.await.unwrap();
 
     Ok(())
 }
